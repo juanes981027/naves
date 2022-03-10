12f3a98f9727b726f8f68c77be90c374d4123d18
juanes981027 <71131466+juanes981027@users.noreply.github.com>
2022-03-09 20:20:37 -0500

    Add files via upload
    Nave se mueve

--- funciones.js
+++ funciones.js
@@ -16,8 +16,49 @@ document.addEventListener("DOMContentLoaded",function(){
 
    
 
-    aliens.forEach(alien => cuadrosTablero[posicionAliens+alien].classList.add("aliens"))
+    // aliens.forEach(alien => cuadrosTablero[posicionAliens+alien].classList.add("aliens"))
+
+    function ubicarAliens(){
+        for(let index = 0; index < aliens.length; index++){
+            cuadrosTablero[aliens[index]].classList.add("aliens")
+        }
+
+    }
+
+    ubicarAliens();
+
+    function quitarAliens(){
+        for(let index = 0; index < aliens.length; index++){
+            cuadrosTablero[aliens[index]].classList.remove("aliens")
+        }
+
+    }
+    
 
     cuadrosTablero[posicionNave].classList.add('nave')
+  
+    // para mover la nave a los lados
+
+    function moverNave(evento){
+
+        cuadrosTablero[posicionNave].classList.remove('nave')
+        switch(evento.key){
+            case "ArrowLeft":
+                if(posicionNave % cuadros !== 0){
+                    posicionNave -= 1;
+                }
+            break
+            case "ArrowRight":
+                if((posicionNave % cuadros) < cuadros -1){
+                    posicionNave += 1;
+                }
+            break;
+            
+            
+        }
+        cuadrosTablero[posicionNave].classList.add("nave")
+    }
+    document.addEventListener('keydown',moverNave)
     
+
 })
